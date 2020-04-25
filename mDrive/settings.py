59707b9bbaf441b55568c0b6c7f60ef768e6ce4f
Mohitesh Ch Agarwal <agarwalmohitesh@gmail.com>
2020-04-25 10:56:52 +0530

    Changed

--- mDrive/settings.py
+++ mDrive/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '08ogzie-sm*whk!_v6&6b@yvixh_xe^0s$sl015v=#q)z_0(dm'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
